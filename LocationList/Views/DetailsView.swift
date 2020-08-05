15a48d66bba14b039f5eed6d4e507a21a4a5d1a8
Anamika Deb <anamika@Anamikas-MacBook-Pro.local>
2020-08-05 16:03:28 +0600

    first commit

---
+++ LocationList/Views/DetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsView.swift
+//  LocationList
+//
+//  Created by Anamika Deb on 5/8/20.
+//  Copyright © 2020 Anamika Deb. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailsView()
+    }
+}
